d6f63379225a7bea7900f21c2ef8c4eb58fbb2b1
Paola Lopez <lopezmezakarlapaola@gmail.com>
2021-06-25 08:59:40 -0500

    1er instantanea, 1 solo webform1 ya hay consulta a SQL server

---
+++ WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebPruebaAccesoSQL.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <script src="js/sweetalert2.all.min.js"></script>
+    <link href="Css/sweetalert2.min.css" rel="stylesheet" />
+    <script src="js/Micodigo.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Prueba Conexion" Width="321px" />
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" Width="700px"></asp:TextBox>
+        </div>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Consuta DataReader" />
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" Height="138px" Width="250px"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Consulta DataSeT" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Obtener datos del DataSet" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:GridView ID="GridView1" runat="server" Height="138px" Width="259px">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using ClassMiAcessoSQL;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebPruebaAccesoSQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        ClassAccesoSQL objacc = null;
+      
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                objacc = new ClassAccesoSQL(@"Data Source = LAPTOP-SFMTQ4SG\SQLEXPRESS; Initial Catalog= BDTIENDA; Integrated Security = true;");
+                Session["objacc"] = objacc;
+            }
+            else
+            {
+                objacc = (ClassAccesoSQL)Session["objacc"];
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection temp = null;
+            string mensaje = " ";
+            temp = objacc.AbrirConexion(ref mensaje);
+            //  TextBox1.Text = mensaje;
+            if (temp != null)
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                    "123", "msgbox('Correcto', '" + mensaje + "', 'success')", true);
+                temp.Close(); //Cerrar conexion 
+                temp.Dispose(); //Destruir la conexion
+            }
+            else
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                   "1234", "msgbox('Incorrecto', '" + mensaje + "', 'error')", true);
+
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlDataReader caja = null;
+            SqlConnection cnab = null;
+            string m = "";
+            cnab = objacc.AbrirConexion(ref m);
+            //  TextBox1.Text = mensaje;
+            if (cnab != null)
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                    "123", "msgbox3('Correcto', '" + m + "', 'success')", true);
+                caja = objacc.ConsultarReader("select * from EMPLEADO", cnab, ref m);
+                if(caja!= null)
+                {
+                    ListBox1.Items.Clear();
+                   while(caja.Read())
+                    {
+                        ListBox1.Items.Add(caja[0] + "" + caja["NOMBRE"]);
+                    }
+                }
+                else
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(),
+                       "mess77", "msgbox3('Peligro', '" + m + "', 'error')", true);
+                }
+                cnab.Close();
+                cnab.Dispose();
+
+
+
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(),
+                   "messg3B", "msgbox3('Peligro', '" + m + "', 'error')", true);
+
+            }
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            DataSet contenedor = null;
+            SqlConnection cnab = null;
+            string m = "";
+            cnab = objacc.AbrirConexion(ref m);
+            //  TextBox1.Text = mensaje;
+            if (cnab != null)
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(),
+                    "messg3B", "msgbox3('Correcto', '" + m + "', 'success')", true);
+                contenedor = objacc.ConsultaDS("select * from EMPLEADO", cnab, ref m);
+                cnab.Close();
+                cnab.Dispose();
+                if (contenedor != null)
+                {
+                    //la consulta es correcta y se muestran los datos
+                    GridView1.DataSource = contenedor.Tables[0];
+                    GridView1.DataBind();
+                    Session["Tabla1"] = contenedor;
+                }
+                else
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(),
+                       "mess77", "msgbox3('Peligro', '" + m + "', 'error')", true);
+                }
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(),
+                   "messg88", "msgbox3('Peligro', '" + m + "', 'error')", true);
+
+            }
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            DataSet t = Session["Tabla1"] as DataSet;
+            ListBox1.Items.Clear();
+            ListBox1.Items.Add("Datos recuperados del dataTable 0");
+            DataRow registro = null;
+            for(int w=t.Tables[0].Rows.Count-1; w>=0; w--)
+            {
+                registro = t.Tables[0].Rows[w];
+                ListBox1.Items.Add(registro[0] + " -- " + registro[1]);
+            }
+        }
+    }
+}
